81139736a52418978cb5850ab5562ef8dce8d973
Ganesh Rajure <53991989+grajure@users.noreply.github.com>
2019-08-11 14:43:58 +0530

    Update PuneWorld.java
    First Correct Java program example....by jyo

--- PuneWorld.java
+++ PuneWorld.java
@@ -2,8 +2,9 @@
 I am staying in pune form last 10 years onwords.
 I belongs to Nanded district.*/
 
-class HelloWorld{
+class HelloWorld1{
   public static void main(String args[]){
-    System.out.println("Hello World");
+    System.out.println("Congratulation Ganesh, \"you had successfully run this program\"");
+
   }
 }
