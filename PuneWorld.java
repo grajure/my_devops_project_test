bd1d6dba56c0618ce24a0c65f0e578f9f9416470
Ganesh Rajure <53991989+grajure@users.noreply.github.com>
2019-08-11 14:18:38 +0530

    Update PuneWorld.java
    Added Hello World printing

--- PuneWorld.java
+++ PuneWorld.java
@@ -1,3 +1,9 @@
-Hi I am Ganesh Rajure, 
+/**Hi I am Ganesh Rajure, 
 I am staying in pune form last 10 years onwords.
-I belongs to Nanded district.
+I belongs to Nanded district.*/
+
+class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("Hello World");
+  }
+}
