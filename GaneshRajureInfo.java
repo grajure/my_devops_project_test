473a3133c0db4f2c616d8ec8ea1406f05b370609
Ganesh Rajure <53991989+grajure@users.noreply.github.com>
2019-08-11 15:02:56 +0530

    Update and rename PuneWorld.java to GaneshRajureInfo.java
    My info

---
+++ GaneshRajureInfo.java
@@ -0,0 +1,5 @@
+class GaneshRajureInfo{
+  public static void main(String args[]){
+    System.out.println("Hi I am Ganesh Rajure. \"I belongs to Nanded district.\" \"I am staying in pune form last 10 years onwords,\" \"I am IT engineer.\"");
+  }
+}
